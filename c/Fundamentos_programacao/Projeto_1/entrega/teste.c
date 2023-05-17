b9e608d80c3248c50c5d62b3d1e7d3ad29940e66
Darigaf <mateus.bernardo@protonmail.com>
2023-05-16 22:24:06 -0300

    started part 2

--- c/Fundamentos_programacao/Projeto_1/entrega/teste.c
+++ c/Fundamentos_programacao/Projeto_1/entrega/teste.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int pegaProximoPixel(int contador){
-	int pixeis[] = {0x12, 0xAF, 0x78, 0xBC, 0x33, 0x51, 0x49, 0xD1, 0xFFFFFFFF};
+	long int pixeis[] = {0x12, 0xAF, 0x78, 0xBC, 0x33, 0x51, 0x49, 0xD1, 0xFFFFFFFF};
 	return pixeis[contador];
 }
 void codificaStreamImagem (int n_bits){
@@ -33,7 +33,7 @@ void codificaStreamImagem (int n_bits){
         while(pixel != 0xFFFFFFFF){
 
 		//quantidade_iteracoes é a quantidade de vezes que o loop precisa ser executado para se obter um byte completo
-		for(int i = 0; i<quantidade_iteracoes; i++){
+		for(int i = 0; i < quantidade_iteracoes; i++){
 			// Compara o byte com a mascara e retorna o valor dos bits mais significativos
 			bits_significativos = pixel & comparison_mask;
 			// Adiciona os bits na variavel
@@ -46,7 +46,7 @@ void codificaStreamImagem (int n_bits){
 		}	
 
                 // Move os bits para a direita, para que fiquem na posição devida.
-                compressed_byte = compressed_byte >> n_bits*2;
+                compressed_byte = compressed_byte >> (n_bits*quantidade_iteracoes);
                 printf("%X \n", compressed_byte);
                 //enviaByteRBD(compressed_byte);
         	compressed_byte = 0x00;
@@ -55,7 +55,57 @@ void codificaStreamImagem (int n_bits){
 
 }
 
+int pegaProximoByteRBD(int contador){
+	long int pixeis[] = {0x1A, 0x7B, 0x35, 0x4D, 0xFFFFFFFF};
+	return pixeis[contador];
+}
+void decodificaStreamRBD (int n_bits, int preenche){
+	int contador = 0;
+	int quantidade_iteracoes;
+	unsigned int uncompressed_byte;
+        unsigned long int comparison_mask;
+        unsigned long int bits_significativos = 0x00;
+	unsigned int compressed_byte = pegaProximoByteRBD(contador);
+	contador++;
+	if(n_bits == 1){
+		quantidade_iteracoes = 8;
+		//1000 0000
+		comparison_mask = 0x80;
+	}
+	else if(n_bits == 2){
+		quantidade_iteracoes = 4;
+		//1100 0000
+		comparison_mask = 0xC0;
+	}
+	else{
+		quantidade_iteracoes = 2;
+		//1111 0000
+		comparison_mask = 0xF0;
+	}
+	
+	while(compressed_byte != 0xFFFFFFFF){
+		
+		for(int i = 0; i < (quantidade_iteracoes-1); i++){
+			// Compara o byte com a mascara e retorna o valor dos bits mais significativos
+			bits_significativos = compressed_byte & comparison_mask;
+			// Adiciona os bits na variavel
+			uncompressed_byte = compressed_byte | bits_significativos;
+			// Move os bits para a esquerda para que seja possível fazer a próxima comparação
+			uncompressed_byte = compressed_byte << (n_bits*quantidade_iteracoes);
+			//printf("%X \n", bits_significativos);
+			compressed_byte = pegaProximoByteRBD(contador);
+			contador ++;
+		}	
+			uncompressed_byte = uncompressed_byte >> (n_bits*quantidade_iteracoes);
+			printf("%X \n", uncompressed_byte);
+		//enviaPixel(uncompressed_byte);
+	}
+}
+ 
+
 int main(){
-	codificaStreamImagem(4);
+	int n_bits = 4;
+	codificaStreamImagem(n_bits);
+	decodificaStreamRBD(n_bits, 0);
 	return 0;
 }
