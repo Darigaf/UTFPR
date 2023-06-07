7522ae11a0f011c412762d91617620e4bd209119
Mateus <faculdade@mateusbernardo.xyz>
2023-06-07 20:53:54 -0300

    added projeto 2

--- c/Fundamentos_programacao/Projeto_2/entrega/t2-2556618-2556693.c
+++ c/Fundamentos_programacao/Projeto_2/entrega/t2-2556618-2556693.c
@@ -1,26 +1,38 @@
 // Trabalho feito pelos alunos Mateus Bernardo - A2556618 & Vitor Ribeiro Gomes - A2556693
 
+#include "trabalho2.h"
+
+//05
 void mudaGanho (double* dados, int n_amostras, double ganho){
-
-
+	for(int i = 0; i < n_amostras; i++){
+		dados[i] *=ganho;
+	}
 }
 
+//05
 void misturaDados (double* dados1, double* dados2, double* saida,
 int n_amostras){
-
-
+	for(int i = 0; i < n_amostras; i++){
+		saida[i] = (dados1[i]+dados2[i]);
+	}
 }
 
+//15
 void inverteSinal (double* dados, int n_amostras){
 
 
+
 }
 
+//35
 void atrasaSinal (double* dados, int n_amostras, int atraso){
-
+	for(int i = n_amostras; i > 0; i--){
+		dados[i] = dados[i-atraso];
+	}
 
 }
 
+//35
 void filtroDaMedia (double* dados, int n_amostras, int largura){
 
 
