e3eb9bc3efcda47ac0b66ca8c79b3de93e5303fe
Darigaf <mateus.bernardo@protonmail.com>
2024-11-30 12:14:03 -0300

    exercicio 1 aula 3 ED1, incompleto exercicio 2 aula 3 ED1

--- 2_Periodo/Estrutura_de_Dados_I/2024_10_21_Aula_3_Estrutura_Pilha/arquivos/elimina-par.c
+++ 2_Periodo/Estrutura_de_Dados_I/2024_10_21_Aula_3_Estrutura_Pilha/arquivos/elimina-par.c
@@ -1,14 +1,31 @@
 #include "stack.h"
 
 int main () {
-  int tam = 6;
-  Stack *s = create (tam);
-  srand(time(NULL));
-  int i;
-  for (i = 0; i < tam; i++) {
-    push (s, rand()%10);
-  }
-  print (s);
-  /*TERMINAR*/
-  destroy (s);
+	int tam = 6;
+	Stack *s = create (tam);
+	srand(time(NULL));
+	int i;
+	for (i = 0; i < tam; i++) {
+		push (s, rand()%10);
+	}
+	print (s);
+
+
+	Stack *temp = create(tam);
+
+	while(!(empty(s))){
+		if((get_peek(s)%2) == 0){
+			push(temp, (pop(s)));
+		}
+		if((get_peek(s)%2) == 1){
+			pop(s);
+		}
+	}
+	while(!(empty(temp))){
+			push(s, (pop(temp)));
+	}
+	print (s);
+
+	/*TERMINAR*/
+	destroy (s);
 }
--- 2_Periodo/Estrutura_de_Dados_I/2024_10_21_Aula_3_Estrutura_Pilha/arquivos/parser.c
+++ 2_Periodo/Estrutura_de_Dados_I/2024_10_21_Aula_3_Estrutura_Pilha/arquivos/parser.c
@@ -1,13 +1,87 @@
 #include "stack.h"
 
-int parser (char *c) {
-  Stack *s = create(strlen(c));
-  int i = 0;
-  while (c[i] != '\0') {
-    /*Terminar*/
-    /*Utilizar somente as operações push, pop, empty, ... (interface)*/	  
-    i++;    
-  }
+/*int parser (char *c) {
+	Stack *aberto = create(strlen(c));
+	int i = 0;
+	while (c[i] != '\0') {
+		if(c[i] == ' '){
+			continue;
+		}
+		if(c[i] == '('){
+			push(aberto, c[i]);	
+			continue;
+		}
+		if(c[i] == '['){
+			push(aberto, c[i]);	
+			continue;
+		}
+		if(c[i] == ')'){
+			if(empty(aberto)){
+				return 0;
+			}
+			/*
+			if(pop(aberto) == '('){
+				continue;
+			}
+			/
+			if(get_peek(aberto) == '('){
+				pop(aberto);
+				continue;
+			}
+		}
+		if(c[i] == ']'){
+			if(empty(aberto)){
+				return 0;
+			}
+			/*if(pop(aberto) == '['){
+				continue;
+			}
+			/
+			if(get_peek(aberto) == '['){
+				pop(aberto);
+				continue;
+			}
+			return 0;
+		}
+		i++;    
+	}
+	return 1;
+}
+*/
+int parser(char *c) {
+    Stack *aberto = create(strlen(c)); // Criação da pilha
+    int i = 0;
+
+    while (c[i] != '\0') {
+        // Ignorar espaços em branco
+        if (c[i] == ' ') {
+            i++;
+            continue;
+        }
+
+        // Se for um caractere de abertura, empurre para a pilha
+        if (c[i] == '(' || c[i] == '[') {
+            push(aberto, c[i]);
+        } 
+        // Se for um caractere de fechamento
+        else if (c[i] == ')' || c[i] == ']') {
+            // Verifique se a pilha está vazia
+            if (empty(aberto)) {
+                return 0; // Sequência mal formada
+            }
+
+            // Verifique se o topo da pilha é o caractere correspondente de abertura
+            if ((c[i] == ')' && get_peek(aberto) == '(') || (c[i] == ']' && get_peek(aberto) == '[')) {
+                pop(aberto); // Remova o topo da pilha
+            } else {
+                return 0; // Sequência mal formada
+            }
+        }
+        i++; // Avança para o próximo caractere
+    }
+
+    // Se a pilha estiver vazia no final, a sequência está bem formada
+    return empty(aberto) ? 1 : 0;
 }
 
 int main () {
@@ -15,7 +89,8 @@ int main () {
   //char *c = "( [ ) ]";
   //char *c = "( ) [ ]";
   //char *c = "( [ ]";
-  char *c = ") (";
+  //char *c = ") (";
+  char *c = "()";
   if (parser(c)) 
     printf("true  (bem formada)\n");
   else  
